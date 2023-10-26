24c7550ec45c56b4659883c17f7f547d88b4024a
valeriacaro <valeria.caro@estudiantat.upc.edu>
2023-10-26 23:46:54 +0200

    fix_test_api

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,12 +1,12 @@
 import pytest
 from fastapi.testclient import TestClient
-
 from src.app.api import app
 
 
 @pytest.fixture(scope="module", autouse=True)
 def client():
-    # Use the TestClient with a `with` statement to trigger the startup and shutdown events.
+    # Use the TestClient with a `with` statement
+    # to trigger the startup and shutdown events.
     with TestClient(app) as client:
         return client
 
@@ -14,19 +14,18 @@ def client():
 @pytest.fixture
 def payload():
     return {
-        "sentiment": "i like dress",
-        "probabilities": 0.65,
+        "text": "i like dress"
     }
 
 
-def test_root(client):
+def test_root_api(client):
     response = client.get("/")
     json = response.json()
     assert response.status_code == 200
     assert (
-        json["data"]["message"]
-        == "Welcome to Sentiment Analysis "
-           "Clothing Reviews! Please, read the `/docs`!"
+            json["data"]["message"]
+            == "Welcome to Sentiment Analysis "
+               "Clothing Reviews! Please, read the `/docs`!"
     )
     assert json["message"] == "OK"
     assert json["status-code"] == 200
@@ -34,13 +33,22 @@ def test_root(client):
     assert json["url"] == "http://testserver/"
     assert json["timestamp"] is not None
 
-def test_model_prediction(client, payload):
+
+def test_predict_valid_request(client, payload):
     response = client.post("/predict", json=payload)
-    json = response.json()
     assert response.status_code == 200
-    assert json["data"]["prediction"] == 2
-    assert json["message"] == "OK"
-    assert json["status-code"] == 200
-    assert json["method"] == "POST"
-    assert json["url"] == "http://testserver/predict"
-    assert json["timestamp"] is not None
+    data = response.json()
+    assert "sentiment" in data
+    assert "probabilities" in data
+
+
+def test_predict_exception(client):
+    json_not_valid = {"text": ""}
+    response = client.post("/predict", json=json_not_valid)
+    assert response.status_code == 400
+
+
+def test_predict_invalid_input(client):
+    json_not_valid = {"text": [0, 3, 4, 1, 9]}
+    response = client.post("/predict", json=json_not_valid)
+    assert response.status_code == 422
