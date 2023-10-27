df09e3250823fa7a0aed560ce259a80c756f2297
valeriacaro <valeria.caro@estudiantat.upc.edu>
2023-10-27 20:21:49 +0200

    fix_add_comments

--- src/features/prepare.py
+++ src/features/prepare.py
@@ -21,10 +21,13 @@ from src import (
 
 if __name__ == '__main__':
 
+    # define the path to the parameters file
     params_path = ROOT_PATH / "params.yaml"
 
+    # load the raw data
     data = get_data_from_local(ROOT_PATH / 'data' / 'raw' / 'raw_data.csv')
 
+    # load parameters from the yaml file
     with open(params_path, "r", encoding='utf-8') as params_file:
         try:
             params = yaml.safe_load(params_file)
@@ -32,15 +35,18 @@ if __name__ == '__main__':
         except yaml.YAMLError as exc:
             print(exc)
 
+    # process data
     data = clean_df(data)
     data = process_df(data)
 
+    # split data into train and test
     train_data = data.sample(
         frac=params["train_size"],
         random_state=params["random_state"]
     )
     test_data = data.drop(train_data.index)
 
+    # save data
     IS_EXIST = os.path.exists(PROCESSED_TRAIN_DATA_PATH)
     if not IS_EXIST:
         os.makedirs(PROCESSED_TRAIN_DATA_PATH)
